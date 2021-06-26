c1c803e4a9da7dd9f5610567f5512fb7262029a7
Mohamed Irfan <64521698+MohamedIrfanAM@users.noreply.github.com>
2021-06-26 14:27:30 +0530

    Update youtube-autoupload.sh

--- youtube-autoupload.sh
+++ youtube-autoupload.sh
@@ -1,14 +1,28 @@
-# /bin/bash
-mv /home/semeeha/*mp4 /home/semeeha/Upload/
-ls /home/semeeha/Upload/*mp4
+#!/bin/bash
+date > /home/semeeha/Upload/upload_status.txt
+echo "---------------------------------" >> /home/semeeha/Upload/upload_status.txt
+counter=0
+uploaded=0
+mv /home/semeeha/*mp4 /home/semeeha/Upload/ &> /dev/null
+ls /home/semeeha/Upload/*mp4 &> /dev/null
+nmcli con up id "M Network"
+
 if [ $? -ne 0 ]; then
-	clear
-	echo "No Videos Available to Upload"
+	echo "No Videos Available to Upload" >> /home/semeeha/Upload/upload_status.txt
 else
-	clear
+	echo "Videos Found, Uploading Started" >> /home/semeeha/Upload/upload_status.txt
 	videos=$(find /home/semeeha/Upload/*mp4) 
-	for video in "$videos"
+	for video in $videos
 	do 
-		/home/semeeha/Upload/.src/youtubeuploader_linux_amd64 -description "Heavens Preschool Vatanappally" -filename $video
+		/home/semeeha/Upload/.src/youtubeuploader_linux_amd64 -description "Heavens Preschool Vatanappally" -filename $video && rm $video && echo "$video uploaded" >> /home/semeeha/Upload/upload_status.txt && uploaded=$((uploaded+1)) || echo "can't upload $video" >> /home/semeeha/Upload/upload_status.txt
+		counter=$((counter+1))
 	done
 fi
+
+if [ $uploaded -eq $counter ]; then
+	echo "All Videos Uploaded, Powering off the system" >> /home/semeeha/Upload/upload_status.txt
+else
+	echo "Errors occured, couldn't upload some videos" >> /home/semeeha/Upload/upload_status.txt
+fi
+echo "---------------------------------" >> /home/semeeha/Upload/upload_status.txt
+date >> /home/semeeha/Upload/upload_status.txt
