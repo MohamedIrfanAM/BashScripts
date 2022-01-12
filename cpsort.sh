a95e6402f0516e8850d6425861ede8bb95156f73
Mohamed Irfan <64521698+MohamedIrfanAM@users.noreply.github.com>
2022-01-12 15:18:32 +0530

    adding script to remove unmodified files

--- cpsort.sh
+++ cpsort.sh
@@ -2,6 +2,17 @@ function sort
 {
 
 # Codeforces
+
+solutions=$(find /run/media/irfan/Programming/cp/Codeforces*/*cpp)
+if [ $? -eq 0 ]; then
+  while IFS= read -r solution; do
+    if ! [[ $(diff -d -q -I '^/' -I '^ /' $solution ~/.config/cpbooster/template.cpp) ]]; then
+      solution_name=$(echo $solution | sed -e 's/.cpp//')
+      rm -fr $solution_name.*
+    fi
+  done <<< "$solutions"
+fi
+
 mv /run/media/irfan/Programming/cp/Codeforc* /run/media/irfan/Programming/cp/codeforces 
 mmv -r /run/media/irfan/Programming/cp/codeforces/CodeforcesCodeforces\* \#1
 mmv -r /run/media/irfan/Programming/cp/codeforces/'CodeforcesEducationalCodeforcesRound*Ratedfor*' 'EducationalRound#1#2'
